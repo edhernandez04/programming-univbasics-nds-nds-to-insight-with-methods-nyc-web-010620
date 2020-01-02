bf295f45b155990b2cbccbc270b73eda1c177135
Eddie Hernandez <mchh.004@gmail.com>
2020-01-02 02:45:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,6 +20,7 @@ def directors_totals(nds)
   result = {}
   name_index = 0
   director = nds[name_index][:name]
+  result[director] = 0
   while name_index < nds.length do
     result[director] += gross_for_director(gross_total)
     name_index += 1
