18b1ab5629a1b7e75b604059a949f894d2409183
Eddie Hernandez <mchh.004@gmail.com>
2020-01-02 02:36:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,12 +19,16 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  nil
-  index = 0
-  while index < nds.length do
-    director = nds[index]
-    result[director[:name]] = gross_for_director(director)
-    index += 1
+  director_index = 0
+  while director_index < nds.length do
+    director = nds[director_index][:name]
+    movie_index = 0
+    result[director] = 0
+    while movie_index < nds[director_index][:movies].length do
+      result[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    director_index += 1
   end
   result
 end
